1e97df82ee4bcc1a6b93a4a7cf32b571c601f713
cktakasu <macbooker1209@gmail.com>
2026-02-15 21:32:38 +0900

    feat: icon-only mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import { Header } from './components/Header'
-import { Footer } from './components/Footer'
 import { LinkGrid } from './components/LinkGrid'
 import { LinkCard } from './components/LinkCard'
 import { links } from './data/links'
 
 function App() {
     return (
-        <main className="w-[360px] p-5 bg-white min-h-0 rounded-2xl m-0 border border-slate-200 shadow-xl flex flex-col items-center">
-            <Header />
+        <main className="w-fit p-2 bg-white min-h-0 rounded-xl m-0 border border-slate-200 shadow-xl flex flex-col items-center">
             <LinkGrid>
                 {links.map((link, index) => (
                     <LinkCard key={link.id} link={link} index={index} />
                 ))}
             </LinkGrid>
-            <Footer />
         </main>
     )
 }
