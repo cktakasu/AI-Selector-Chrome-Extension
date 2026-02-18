a0094f85c458ef85d865782ae67ee5f312fe26b8
cktakasu <macbooker1209@gmail.com>
2026-02-18 13:37:57 +0900

    style: right-align icons in LinkGrid

--- src/components/LinkGrid.tsx
+++ src/components/LinkGrid.tsx
@@ -6,7 +6,7 @@ interface LinkGridProps {
 
 export function LinkGrid({ children }: LinkGridProps) {
     return (
-        <div className="flex flex-row flex-nowrap gap-0.5 justify-center items-center w-full">
+        <div className="flex flex-row flex-nowrap gap-0.5 justify-end items-center w-full">
             {children}
         </div>
     )
